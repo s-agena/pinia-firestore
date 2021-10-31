435590854e74701830827ce2ef9231142fbb4639
安慶名　真 <shi-agena@nec.com>
2021-10-31 13:57:26 +0900

    Correspond to promise

--- packages/pinia-firestore/src/pinia-firestore.ts
+++ packages/pinia-firestore/src/pinia-firestore.ts
@@ -1,20 +1,33 @@
-
-import Vue from 'vue'
-import { QueryDocumentSnapshot, DocumentSnapshot, SnapshotMetadata, Unsubscribe, onSnapshot, DocumentReference, DocumentData, CollectionReference, Query } from 'firebase/firestore'
-import { StateTree, StoreWithState } from 'pinia'
+import Vue from "vue";
+import {
+  QueryDocumentSnapshot,
+  DocumentSnapshot,
+  SnapshotMetadata,
+  Unsubscribe,
+  onSnapshot,
+  DocumentReference,
+  DocumentData,
+  CollectionReference,
+  Query,
+} from "firebase/firestore";
+import { StateTree, StoreWithState } from "pinia";
 
 //////////////////
 // define firestore type
 //////////////////
 
-export type FirestoreReference = DocumentReference<DocumentData> | CollectionReference<DocumentData> | Query<DocumentData>
+export type FirestoreReference =
+  | DocumentReference<DocumentData>
+  | CollectionReference<DocumentData>
+  | Query<DocumentData>;
 
 //////////////////
 // Output debug log.
 //////////////////
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function debug(message: string, ...data: any[]): void {
-  if (_op.debug) console.log("[pinia-firestore] "+message, data)
+  if (_op.debug) console.log("[pinia-firestore] " + message, data);
 }
 
 //////////////////
@@ -23,14 +36,14 @@ function debug(message: string, ...data: any[]): void {
 
 // define type
 type TypeUnsubscribe = {
-  id: string
-  name: string
-  unsub: Unsubscribe
-  type: string
-}
+  id: string;
+  name: string;
+  unsub: Unsubscribe;
+  type: string;
+};
 
 // keep value
-const unsubs = {} as { [key: string]: TypeUnsubscribe }
+const unsubs = {} as { [key: string]: TypeUnsubscribe };
 
 // access methods
 function store(id: string, name: string, unsub: Unsubscribe, type: string) {
@@ -39,23 +52,23 @@ function store(id: string, name: string, unsub: Unsubscribe, type: string) {
     name,
     unsub,
     type,
-  }
-  unsubs[id + ":" + name] = item
-  debug("store:", item)
+  };
+  unsubs[id + ":" + name] = item;
+  debug("store:", item);
 }
 
 function pick(id: string, name: string): TypeUnsubscribe {
-  const item = unsubs[id + ":" + name]
-  debug("pick:", id, name, item)
-  return item
+  const item = unsubs[id + ":" + name];
+  debug("pick:", id, name, item);
+  return item;
 }
 
 function remove(id: string, name: string) {
-  const item = unsubs[id + ":" + name]
+  const item = unsubs[id + ":" + name];
   if (item !== undefined) {
-    //　If it has already been registered, it will be unsubscribe.
-    delete unsubs[id + ":" + name]
-    debug("remove:", id, name, item)
+    // If it has already been registered, it will be unsubscribe.
+    delete unsubs[id + ":" + name];
+    debug("remove:", id, name, item);
   }
 }
 
@@ -63,100 +76,164 @@ function remove(id: string, name: string) {
 // External public functions.
 //////////////////
 
-function makeDocumentData(snapshot: QueryDocumentSnapshot | DocumentSnapshot): DocumentData {
-  let doc = snapshot.data() || {}
-  doc = Object.defineProperty(doc, '__id', { value: snapshot.id })
-  doc = Object.defineProperty(doc, '__path', { value: snapshot.ref.path })
-  doc = Object.defineProperty(doc, '__metadata', { value: snapshot.metadata })
-  return doc
+export type DocumentProperties = {
+  readonly __id: string;
+  readonly __path: string;
+  readonly __metadata: SnapshotMetadata;
+};
+
+function makeDocumentData(
+  snapshot: QueryDocumentSnapshot | DocumentSnapshot
+): DocumentData {
+  let doc = snapshot.data() || {};
+  doc = Object.defineProperty(doc, "__id", { value: snapshot.id });
+  doc = Object.defineProperty(doc, "__path", { value: snapshot.ref.path });
+  doc = Object.defineProperty(doc, "__metadata", { value: snapshot.metadata });
+  return doc;
 }
 
-export const bind = <ID extends string, S extends StateTree, G, A>
-(piniaInstance: StoreWithState<ID,S,G,A>, field: keyof S, ref: FirestoreReference) => {
-  // Delete bound listen
-  const item = pick(piniaInstance.$id, field.toString())
-  if (item !== undefined) {
-    item.unsub()
-  }
+export const bind = <ID extends string, S extends StateTree, G, A>(
+  piniaInstance: StoreWithState<ID, S, G, A>,
+  field: keyof S,
+  ref: FirestoreReference
+): Promise<void> => {
+  return new Promise((resolve) => {
+    let notified = false;
+    // Delete bound listen
+    const item = pick(piniaInstance.$id, field.toString());
+    if (item !== undefined) {
+      item.unsub();
+    }
 
-  // Receive real-time updates
-  if (ref.type === 'document') {
-    // Receive real-time updates for a single document.
-    const unsub = onSnapshot(ref, (snapshot) => {
-      debug("listen:", piniaInstance.$id, field.toString(), snapshot.data())
-      piniaInstance.$state[field] = makeDocumentData(snapshot) as any
-    },
-    (error) => {
-      debug("error", error)
-      // https://firebase.google.com/docs/firestore/query-data/listen?hl=ja#handle_listen_errors
-      remove(piniaInstance.$id, field.toString())
-    })
-    store(piniaInstance.$id, field.toString(), unsub, ref.type)
-  } else {
-    // working area
-    const docs: DocumentData[] = piniaInstance.$state[field]
-    docs.splice(0, docs.length)
-    // Receive real-time updates for multiple documents.
-    const unsub = onSnapshot(ref, (querySnapshot) => {
-      querySnapshot.docChanges().forEach((change) => {
-        debug("listen:", piniaInstance.$id, field.toString(), change.type, change.doc.data())
-        if (change.type === "added") {
-          docs.splice(change.newIndex, 0, makeDocumentData(change.doc))
-        } else if (change.type === 'modified') {
-          docs.splice(change.newIndex, 1, makeDocumentData(change.doc))
-        } else if (change.type === 'removed') {
-          docs.splice(change.oldIndex, 1)
+    // Receive real-time updates
+    if (ref.type === "document") {
+      // Receive real-time updates for a single document.
+      const unsub = onSnapshot(
+        ref,
+        (snapshot) => {
+          debug(
+            "listen:",
+            piniaInstance.$id,
+            field.toString(),
+            snapshot.data()
+          );
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          piniaInstance.$state[field] = makeDocumentData(snapshot) as any;
+
+          // Notify the completion of the first update
+          if (!notified) {
+            notified = true;
+            resolve();
+          }
+        },
+        (error) => {
+          debug("error", error);
+          // https://firebase.google.com/docs/firestore/query-data/listen?hl=ja#handle_listen_errors
+          remove(piniaInstance.$id, field.toString());
+
+          // Notify the completion of the first update
+          if (!notified) {
+            notified = true;
+            resolve();
+          }
         }
-      })
-    },
-    (error) => {
-      debug("error", error)
-      // https://firebase.google.com/docs/firestore/query-data/listen?hl=ja#handle_listen_errors
-      remove(piniaInstance.$id, field.toString())
-    })
-    store(piniaInstance.$id, field.toString(), unsub, ref.type)    
-  }
-}
+      );
+      store(piniaInstance.$id, field.toString(), unsub, ref.type);
+    } else {
+      // working area
+      const docs: DocumentData[] = piniaInstance.$state[field];
+      docs.splice(0, docs.length);
+      // Receive real-time updates for multiple documents.
+      const unsub = onSnapshot(
+        ref,
+        (querySnapshot) => {
+          console.log("change len", querySnapshot.docChanges().length);
+          querySnapshot.docChanges().forEach((change) => {
+            debug(
+              "listen:",
+              piniaInstance.$id,
+              field.toString(),
+              change.type,
+              change.doc.data()
+            );
+            if (change.type === "added") {
+              docs.splice(change.newIndex, 0, makeDocumentData(change.doc));
+            } else if (change.type === "modified") {
+              docs.splice(change.newIndex, 1, makeDocumentData(change.doc));
+            } else if (change.type === "removed") {
+              docs.splice(change.oldIndex, 1);
+            }
+          });
+
+          // Notify the completion of the first update
+          if (!notified) {
+            notified = true;
+            resolve();
+          }
+        },
+        (error) => {
+          debug("error", error);
+          // https://firebase.google.com/docs/firestore/query-data/listen?hl=ja#handle_listen_errors
+          remove(piniaInstance.$id, field.toString());
+
+          // Notify the completion of the first update
+          if (!notified) {
+            notified = true;
+            resolve();
+          }
+        }
+      );
+      store(piniaInstance.$id, field.toString(), unsub, ref.type);
+    }
+  });
+};
 
 type UnbindOptions = {
-  reset: boolean
-}
+  reset: boolean;
+};
 
-export const unbind = <ID extends string, S extends StateTree, G, A>
-(piniaInstance: StoreWithState<ID,S,G,A>, field: keyof S, options: UnbindOptions = { reset: true }) => {
-  const item = pick(piniaInstance.$id, field.toString())
+export const unbind = <ID extends string, S extends StateTree, G, A>(
+  piniaInstance: StoreWithState<ID, S, G, A>,
+  field: keyof S,
+  options: UnbindOptions = { reset: true }
+): void => {
+  const item = pick(piniaInstance.$id, field.toString());
   if (item !== undefined) {
-    item.unsub()
-    remove(piniaInstance.$id, field.toString())
+    item.unsub();
+    remove(piniaInstance.$id, field.toString());
 
     // Overwrite with reset value
     if (options.reset) {
-      if (item.type === 'document') {
-        piniaInstance.$state[field] = {} as any
+      if (item.type === "document") {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        piniaInstance.$state[field] = {} as any;
       } else {
-        piniaInstance.$state[field] = [] as any
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        piniaInstance.$state[field] = [] as any;
       }
     }
   }
-}
+};
 
 //////////////////
 // Functions for plugins.
 //////////////////
 
 export type CreateOptions = {
-  debug: boolean
-}
+  debug: boolean;
+};
 
 const _op: CreateOptions = {
-  debug: false
-}
+  debug: false,
+};
 
-export function createPiniaFirestore(options: CreateOptions = { debug: false }) {
+export function createPiniaFirestore(
+  options: CreateOptions = { debug: false }
+): Vue.Plugin {
   return {
-    install: (app: Vue.App) => {
-      _op.debug = options.debug
-      debug("install")
-    }
-  }
+    install: () => {
+      _op.debug = options.debug;
+      debug("install");
+    },
+  };
 }
